ba275eb00b3e7d87f77ffb92f62b1ff360933bcd
Julien Steininger <julien.steininger@etes.de>
2019-10-01 09:38:12 +0200

    Dateien aus niedrigeren Verzeichnissen hinzugefügt

---
+++ src/ContaoCookiebotBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * This file is part of [package name].
+ *
+ * (c) John Doe
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace systemhaus\CookiebotBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ContaoCookiebotBundle extends Bundle
+{
+}
