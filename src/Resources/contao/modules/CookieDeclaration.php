00dec3c76d5fc6af6ca3a2e620754115e09189a9
Julien Steininger <julien.steininger@etes.de>
2019-10-08 12:24:39 +0200

    Platzhalter im Backend eingebaut - Beispieltext sieht so aus: ###COOKIEBOT-ERKLÄRUNG### Cookie-Erklärung-FE-Modul (ID: 82)

--- src/Resources/contao/modules/CookieDeclaration.php
+++ src/Resources/contao/modules/CookieDeclaration.php
@@ -21,6 +21,19 @@ class CookieDeclaration extends Module
 {
     protected $strTemplate = 'mod_cookie_declaration';
 
+    public function generate()
+    {
+        if (TL_MODE === 'BE') {
+            $objTemplate = new \BackendTemplate('be_wildcard');
+            $objTemplate->wildcard = '###' . utf8_strtoupper($GLOBALS['TL_LANG']['FMD']['cookieDeclaration'][0] . '###');
+            $objTemplate->id = $this->id;
+            $objTemplate->link = $this->name;
+            $objTemplate->href = 'contao/main.php?do=themes&amp;table=tl_module&amp;act=edit&amp;id=' . $this->id;
+            return $objTemplate->parse();
+        }
+        return parent::generate();
+    }
+    
     protected function compile()
     {
         global $objPage;
